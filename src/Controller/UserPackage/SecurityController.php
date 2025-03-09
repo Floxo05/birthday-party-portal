8e6d74a1888480b39c8c4d197d141a9d8c761ba6
Florian <chris23wolf@googlemail.com>
2025-03-09 19:17:26 +0100

    login geht noch nicht ganz...

--- src/Controller/UserPackage/SecurityController.php
+++ src/Controller/UserPackage/SecurityController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller\UserPackage;
 
+use App\Entity\UserPackage\User;
 use App\Form\UserPackage\LoginFormType;
+use App\Form\UserPackage\RegisterFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -31,5 +36,25 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $hasher): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegisterFormType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword($hasher->hashPassword($user, $form->get('password')->getData()));
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('security/register.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
